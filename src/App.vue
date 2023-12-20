15a0d04f93dc1b574b540fb48f502f5ff984a418
HANG Eric <eric.hang@meteo.fr>
2023-12-20 17:10:52 -0500

    Init project with vue3 & vuetify using create-vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheSearch from './components/TheSearch.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Weather interview</h1>
+  </header>
+
+  <main>
+    <TheSearch />
+  </main>
+</template>
+
+<style scoped>
+header {
+  background-color: #ddd;
+}
+</style>
