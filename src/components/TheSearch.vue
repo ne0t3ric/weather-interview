f4024ce2c768c9513c71b1551d158d018e788b8b
HANG Eric <eric.hang@meteo.fr>
2023-12-20 17:13:02 -0500

    feat: add input to search address and use it with geocoding API

---
+++ src/components/TheSearch.vue
@@ -0,0 +1,11 @@
+<template>
+  <input v-model="address" type="text">
+</template>
+<script setup lang="ts">
+import {ref, watch} from 'vue'
+import {useGeocodingAPI} from '@/apis/geocoding/useGeocodingAPI'
+const address = ref('')
+const { data, error } = useGeocodingAPI(address)
+
+watch(data, () => console.warn(data))
+</script>
