b479727fc389375a5fde3470c63b2d8d0c512ef1
HANG Eric <eric.hang@meteo.fr>
2023-12-21 16:40:15 -0500

    feat: add cambodia location in suggestions

--- src/components/LocationsSuggestion.vue
+++ src/components/LocationsSuggestion.vue
@@ -16,7 +16,8 @@ const emits = defineEmits(['update:modelValue'])
 const suggestions = [
   {'label': 'Toulouse, France', 'lon': 1.440694, 'lat': 43.603902},
   {'label': 'Paris, France', 'lon': 2.342865, 'lat': 48.858705},
-  {'label': 'Lyon, France', 'lon': 4.835571, 'lat': 45.732398}
+  {'label': 'Lyon, France', 'lon': 4.835571, 'lat': 45.732398},
+  {'label': 'Phnom Penh, Cambodge', 'lon': 104.91601, 'lat': 11.56245}
 ]
 
 
